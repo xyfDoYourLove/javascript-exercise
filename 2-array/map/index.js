e19a970040018c2fa7f75dd924363422c234fcba
DoYourLove <xyf15620168540@163.com>
2020-08-10 16:12:26 +0800

    [XiaoYiFan] feat : 2-array map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,19 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  return collection.map(item => {
+    switch (item) {
+      case 1:
+        return 'a';
+      case 2:
+        return 'b';
+      case 3:
+        return 'c';
+      case 4:
+        return 'd';
+      case 5:
+        return 'e';
+      default:
+        return '';
+    }
+  });
 }
