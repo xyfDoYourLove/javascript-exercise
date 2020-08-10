db3702c82fbd918a2c41cf8ca54fdfe3216d02a6
DoYourLove <xyf15620168540@163.com>
2020-08-10 22:45:07 +0800

    [XiaoYiFan] feat : finish 5-async 2-promise

--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,7 +3,19 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('content-type', 'application/json');
 
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+        resolve(xhr.response);
+      }
+      if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+        reject(xhr.response);
+      }
+    };
+
+    xhr.send();
     // end -->
   });
 }
