eefb70964b8c952b98509eee6e33e4f92b8acbdf
DoYourLove <xyf15620168540@163.com>
2020-08-10 23:10:47 +0800

    [XiaoYiFan] feat : 5-async 3-fetch

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,13 +1,21 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  const option = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  return fetch(url, option);
   // end -->
 }
 
 const URL = 'http://localhost:3000/api';
 fetchData(URL)
   .then(result => {
-    document.writeln(result.name);
+    result.json().then(data => document.writeln(data.name));
+    // document.writeln(result.name);
   })
   .catch(error => {
     console.error(error);
