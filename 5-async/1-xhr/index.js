2c6c6e416c625ff870089160d5169580328f32be
DoYourLove <xyf15620168540@163.com>
2020-08-10 22:40:54 +0800

    [XiaoYiFan] feat : finish 5-async 1-xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,19 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
+  xhr.open('GET', url, true);
+  xhr.setRequestHeader('content-type', 'application/json');
 
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+      successCallback(xhr.response);
+    }
+    if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+      errorCallback(xhr.response);
+    }
+  };
+
+  xhr.send();
   // end -->
 }
 
