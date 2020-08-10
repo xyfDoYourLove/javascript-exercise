bafc7f296cd41e6d85309451cd15fb33c5763fb5
DoYourLove <xyf15620168540@163.com>
2020-08-10 23:28:37 +0800

    [XiaoYiFan] feat : 5-async 4-async

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,15 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const option = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  const result = await fetch(url, option);
+  const jsonData = await result.json();
+  document.writeln(jsonData.name);
   // end -->
 }
 
