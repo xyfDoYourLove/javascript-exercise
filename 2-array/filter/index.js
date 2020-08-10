82fd454daa855392de23dd86f0ae84e9dc464505
DoYourLove <xyf15620168540@163.com>
2020-08-10 15:58:42 +0800

    [XiaoYiFan] feat ：finish 1-array filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,18 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(item => item % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  let repeat = 0;
+  return collection.filter(item => {
+    if (item !== repeat) {
+      repeat = item;
+      return true;
+    }
+    return false;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
