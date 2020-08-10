84151b77faceb35c0897aee918a94e75a900a5b2
DoYourLove <xyf15620168540@163.com>
2020-08-10 21:01:20 +0800

    [XiaoYiFan] feat : finish 4-class practice_3

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,13 @@
+import Person from './person';
+
 // TODO 13: 在这里写实现代码
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
