8535419fc6ea6131f4c961f599f757e5fc7c2532
DoYourLove <xyf15620168540@163.com>
2020-08-10 15:38:36 +0800

    [XiaoYiFan] feat : finish 1-string  startsWith

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const filteredConection = collection.filter(item => item.startsWith('粤A'));
+  return filteredConection.length;
 }
