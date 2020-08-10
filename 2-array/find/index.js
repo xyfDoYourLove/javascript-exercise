ff6a5280a0cde281d36b67dfdbd666bc124109ad
DoYourLove <xyf15620168540@163.com>
2020-08-10 16:04:27 +0800

    [XiaoYiFan] feat : finish 2-array find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,5 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const oooldPerson = collection.filter(item => item.age > 10 && item.age <= 20);
+  return oooldPerson[0].name;
 }
