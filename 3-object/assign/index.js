47be6989f5be0827a5ee8326b8fc742139466bb7
DoYourLove <xyf15620168540@163.com>
2020-08-10 16:19:51 +0800

    [XiaoYiFan] feat : 3-object assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,9 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const target = { serialNumber: '12345' };
+  const source2 = { properties: { color: 'green', status: 'processed' } };
+
+  Object.assign(target, source, source2);
+
+  return target;
 }
