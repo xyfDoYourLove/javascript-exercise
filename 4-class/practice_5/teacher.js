d56fe008e30b070ab1e46079138de9e02b566cbc
DoYourLove <xyf15620168540@163.com>
2020-08-10 21:27:27 +0800

    [XiaoYiFan] feat : finish 4-class practice_5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
+import Person from './person';
 // TODO 20: 在这里写实现代码
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (!this.klass) {
+      return `${super.introduce()} I am a Teacher. I teach No Class.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}
