4da63a22dfdd95dfe2803369e4a2d7cede6ca0fd
DoYourLove <xyf15620168540@163.com>
2020-08-10 20:10:29 +0800

    [XiaoYiFan] feat : 3-object values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source).reduce(
+    (accumulator, currentValue) => accumulator * 1 + currentValue * 1
+  );
 }
