7861be6982fa5fc8a8ac06cb38c233e4138312ac
DoYourLove <xyf15620168540@163.com>
2020-08-10 16:06:32 +0800

    [XiaoYiFan] fix : fix 1-array find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,5 +1,4 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
-  const oooldPerson = collection.filter(item => item.age > 10 && item.age <= 20);
-  return oooldPerson[0].name;
+  return collection.find(item => item.age > 10 && item.age <= 20).name;
 }
